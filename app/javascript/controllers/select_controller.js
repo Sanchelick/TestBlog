d10bc4a922a8fcc50c722ecca3b4d58d405abe98
Oleg Chechil <che-4ILL@yandex.ru>
2026-03-23 20:05:35 +0300

    Версия с тегами

--- app/javascript/controllers/select_controller.js
+++ app/javascript/controllers/select_controller.js
@@ -1,37 +1,11 @@
-import TomSelect from 'tom-select/dist/js/tom-select.popular'
+import { Controller } from "@hotwired/stimulus"
+import TomSelect from "tom-select"
 import Translations from './i18n/select.json'
 
-document.addEventListener('turbolinks:load', function() {
-    const i18n Translations[document.querySelector('body').dataset.lang]
-    
-    document.querySelectorAll('.js-multiple-select').forEach(element) => {
-	let opts = {
-	    plugins:{
-		'remove_button': {
-		    title: i18n["remove_button"]},
-		'no_backspace_delete': {},
-		'restore_on_backspace': {}
-	    },
-	    valueField: 'id',
-	    labelField: 'title',
-	    searchField: 'title',
-	    create: false,
-	    load: function(query, callback) {
-		const url element.dataset.ajaxUrl + 'json?term=' + encodeURIComponent(query)
-
-		fetch(url)
-		    .then(responce => responce.json())
-		    .then(json => { callback(json) })
-		    .catch(() => { callback() })
-	    },
-	    render: {
-		no_results: function(_data, _escape) {
-		    return '<div class="no-results">' + i18n['no_results'] + '</div>';
-		}
-		
-	    }
-	}
-
-    new TomSelect(element, opts)
+export default class extends Controller {
+  // We bind the select to tom-select on connect
+  connect() {
+    new TomSelect(this.element)
+  }
 }
     
